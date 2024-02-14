750a1e21e718c2a0a75e88bfb29f1268d11e767d
iefegensh <gefeis3@uw.edu>
2024-02-13 18:15:42 -0800

    finish view.js

--- problem-c/js/View.js
+++ problem-c/js/View.js
@@ -1,3 +1,40 @@
 "use stirct";
 
-import { getIncompleteTasks } from './Model.js';
+import {getIncompleteTasks} from './Model.js';
+
+let renderSingleTask = function(task, markCompleteCallback) {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.textContent = ` ${task.description}`;
+
+    let button = document.createElement("button");
+    button.classList.add("btn" ,"btn-sm", "btn-warning");
+    button.innerHTML = '<span class="material-icons-outlined">done</span>';
+    button.addEventListener("click", () => {
+        if(markCompleteCallback) {
+            markCompleteCallback(task);
+        }
+    });
+
+    li.prepend(button);
+    return li;
+}
+
+export function renderTaskList(markCompleteCallback) {
+    let ul = document.createElement("ul");
+    ul.classList.add("list-group", "list-group-flush");
+
+    let tasks = getIncompleteTasks();
+    if (tasks.length === 0) {
+        let div = document.createElement("div");
+        div.textContent = "No tasks!";
+        return div;
+    }
+
+    tasks.forEach(task => {
+        let li = renderSingleTask(task, markCompleteCallback);
+        ul.appendChild(li);
+    })
+
+    return ul;
+}
